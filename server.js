83f683f2454f1db70752b03804da9e5699972376
Ankit Keshari <ankit.keshari@gslab.com>
2020-06-04 10:43:02 +0530

    update in package

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ const app = express();
 app.use(express.static(__dirname + '/dist/lms-frontend'));
 
 app.get('/*', function(req,res) {
-
 res.sendFile(path.join(__dirname+'/dist/lms-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, (err) =>{
+    if (!err) {
+        console.log(`Server is running on port ${PORT}.`);
+    } else {
+        console.log(`Error :: ${err}.`);
+    }
+});
